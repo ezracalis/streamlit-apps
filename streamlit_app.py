70beac821ba397c128ad0b41ad67362d7c2d4557
Ezra Calis <ezra.calis@gmail.com>
2023-09-10 20:32:00 +0800

    capstone

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ user_input_container = st.empty()
 sentence = user_input_container.text_area(label='Input a sentence in Malay to determine its sentiment.')
 
 # Display the inputs
-user_input = {"Sentence": sentence}
+user_input = {"Sentence": [sentence]}
 st.write("User input:")
 st.write(user_input)
 
@@ -45,7 +45,7 @@ api_route = '/predict_sentiment'
 
 # Add a submit button
 if st.button("Submit"): # only display model predictions on UI if user clicks "Submit" button
-    response = requests.post(f'{api_url}{api_route}', json=json.dumps(user_input)) # json.dumps() converts dict to JSON
+    response = requests.post(f'{api_url}{api_route}', json=user_input)
     predictions = response.json() # return dictionary with key 'predictions' & values are a list of predictions
     
     st.write(f"Prediction: {predictions['predictions'][0]}") # prediction values were stored in 'predictions' key of dict: predictions. [0] is to give prediction output 0/1 in a "unlisted" format since we're only sending user inputs for 1 row of X at a time
