8abf0bc932f2296296e1f53e94806270fa7443fd
Ezra Calis <ezra.calis@gmail.com>
2023-09-10 20:47:52 +0800

    capstone

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import requests
 import json
-import matplotlib.pyplot as plt
 
 # Title of the webpage
 st.title("Sentiment Analysis (Bahasa Melayu 🇸🇬🇲🇾🇧🇳🇮🇩)")
@@ -17,27 +16,6 @@ user_input = {"sentences": [sentence]}
 st.write("User input:")
 st.write(user_input)
 
-# Create buttons with words
-button1 = st.button("Tahniah!")
-button2 = st.button("Ape kau nyer problem eh")
-button3 = st.button("Eh musibot, lu rilek sua")
-
-# Define content for each button
-content1 = "Tahniah!"
-content2 = "Ape kau nyer problem eh"
-content3 = "Eh musibot, lu rilek sua"
-
-# Update the original text area based on the button clicked
-if button1:
-    user_input_container.text_area("Input a sentence in Malay to determine its sentiment.", content1)
-    sentence = content1  # Update the 'sentence' variable
-if button2:
-    user_input_container.text_area("Input a sentence in Malay to determine its sentiment.", content2)
-    sentence = content2  # Update the 'sentence' variable
-if button3:
-    user_input_container.text_area("Input a sentence in Malay to determine its sentiment.", content3)
-    sentence = content3  # Update the 'sentence' variable
-
 # Code to post the user inputs to the API and get the predictions
 # Paste the URL to your GCP Cloud Run API here!
 api_url = 'https://malay-sentiment-nqaybcgjca-as.a.run.app'  # replace with own Google Cloud Run API
@@ -47,18 +25,5 @@ api_route = '/predict_sentiment'
 if st.button("Submit"):  # only display model predictions on UI if user clicks "Submit" button
     response = requests.post(f'{api_url}{api_route}', json=user_input)
     predictions = response.json()  # return dictionary with key 'predictions' & values are a list of predictions
-
-    # Create a colorful bar chart to visualize sentiment probability
-    plt.figure(figsize=(8, 4))
-    plt.bar(['Negative', 'Positive'], predictions["probability"], color=['red', 'green'])
-    plt.xlabel('Sentiment')
-    plt.ylabel('Probability')
-    plt.title('Sentiment Analysis Result')
     
-    # Display the bar chart in Streamlit
-    st.pyplot(plt)
-
-    # Display the sentiment label and probability
-    sentiment_label = "Positive" if predictions["sentiment"] == "positive" else "Negative"
-    st.write(f"Sentiment: {sentiment_label}")
-    st.write(f"Probability: {round(predictions['probability'][0] * 100, 2)}%")
+    st.write(f"Prediction: {predictions}")  # prediction values were stored in 'predictions' key of dict: predictions. [0] is to give prediction output 0/1 in a "unlisted" format since we're only sending user inputs for 1 row of X at a time
