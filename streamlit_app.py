6407e4824f666e24a4137816a7a1937f08f2a981
Ezra Calis <ezra.calis@gmail.com>
2023-09-10 21:08:58 +0800

    capstone

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import requests
 import json
-import pandas as pd
 
 # Title of the webpage
 st.title("Sentiment Analysis (Bahasa Melayu 🇸🇬🇲🇾🇧🇳🇮🇩)")
@@ -29,25 +28,13 @@ if st.button("Submit"):  # only display model predictions on UI if user clicks "
     
     st.write(f"Prediction: {predictions}")  # prediction values were stored in 'predictions' key of dict: predictions. [0] is to give prediction output 0/1 in a "unlisted" format since we're only sending user inputs for 1 row of X at a time
    # Extract the sentiment probability from the dictionary
-    # Extract the sentiment probability from the dictionary
-    probability = predictions["probability"][0]
+    probability = predictions["probability"]
 
-    # Create a DataFrame for the chart
-    chart_data = pd.DataFrame({
+    # Create a dictionary to pass to st.bar_chart and round the values
+    chart_data = {
         "Sentiment": ["Negative", "Not Negative"],
-        "Probability": [probability, 1 - probability],
-        "Color": ["Negative", "Not Negative"]
-    })
+        "Probability": [probability, 1 - probability]
+    }
 
-    # Create a custom bar chart using Altair
-    chart = alt.Chart(chart_data).mark_bar().encode(
-        x="Sentiment",
-        y="Probability",
-        color=alt.Color("Color", scale=alt.Scale(domain=["Negative", "Not Negative"], range=["#FC8282", "#82E0FC"])),
-        tooltip=["Sentiment", "Probability"]
-    ).properties(
-        width=alt.Step(80)
-    )
-
-    # Display the chart
-    st.altair_chart(chart, use_container_width=True)
+    # Create a bar chart
+    st.bar_chart(chart_data, x="Sentiment", y="Probability", use_container_width=True)
